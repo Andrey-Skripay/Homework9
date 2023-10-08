58c00d4df0399c3d54ebd23c912faf5fcd7c0416
Andrey.Skripay <lordktan@gmail.com>
2023-10-08 19:13:32 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        // Задача 1
+
         int[] payments = {1500, 2000, 3000, 1200, 2500}; //  массив с выплатами
 
         int sum = 0;
