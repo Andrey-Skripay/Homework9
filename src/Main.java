39f86c39e33206b76d20ff9b1438011e53f4f6a8
Andrey.Skripay <lordktan@gmail.com>
2023-10-08 19:19:19 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,27 @@ public class Main {
         }
 
         System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+        //Задача 2
+
+        int[] payments1 = {1500, 200, 5000, 1200, 2500};
+
+        int minPayment = payments1[0];
+        int maxPayment = payments1[0];
+
+        for (int i = 1; i < payments1.length; i++) {
+            if (payments1[i] < minPayment) {
+                minPayment = payments1[i];
+            }
+            if (payments1[i] > maxPayment) {
+                maxPayment = payments1[i];
+            }
+        }
+
+        System.out.println("Минимальная сумма трат за день составила " + minPayment + " рублей.");
+        System.out.println("Максимальная сумма трат за день составила " + maxPayment + " рублей.");
+
     }
+
+
 }
