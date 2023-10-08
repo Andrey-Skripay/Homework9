6d60f9fecff51a9c81544c6e93f5fd1843e5484c
Andrey.Skripay <lordktan@gmail.com>
2023-10-08 19:10:37 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] payments = {1500, 2000, 3000, 1200, 2500}; //  массив с выплатами
+
+        int sum = 0;
+        for (int payment : payments) {
+            sum += payment;
+        }
+
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
     }
 }
